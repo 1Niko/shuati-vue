8ff450339e86fd7a6b77fabb5fd451fe795b6b9c
xseven <xseuen@163.com>
2020-12-14 13:10:56 +0800

    vue练手项目 Init

---
+++ src/views/Login.vue
@@ -0,0 +1,157 @@
+<template>
+  <!--登录表单的容器-->
+  <div class="login_container">
+    <!--登录区域-->
+    <div class="login_box">
+      <!--头像-->
+      <div class="avatar_box">
+        <img src="../assets/img/索隆.gif">
+      </div>
+      <!--表单-->
+      <el-form :model="loginForm" status-icon :rules="loginRules" ref="loginForm"  class="login_form">
+        <el-form-item  prop="username">
+          <el-input type="text" prefix-icon="el-icon-user-solid" v-model="loginForm.username" autocomplete="on" placeholder="用户名"></el-input>
+        </el-form-item>
+        <el-form-item  prop="password">
+          <el-input type="password" prefix-icon="el-icon-lock" v-model="loginForm.password" autocomplete="off" placeholder="密码"></el-input>
+        </el-form-item>
+        <el-form-item  prop="verifyCode">
+          <div class="verify_box">
+            <el-input type="text" prefix-icon="el-icon-mobile" v-model="loginForm.verifyCode" autocomplete="off" placeholder="验证码" class="verify_code"></el-input>
+            <img src="https://s2.ax1x.com/2019/08/23/msFXK1.gif" alt="验证码" class="verify_img">
+          </div>
+        </el-form-item>
+        <el-form-item class="login_btn">
+          <el-button type="primary" @click="submitForm('loginForm')">登录</el-button>
+          <el-button @click="resetForm('loginForm')">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data() {
+    let validateName = (rule, value, callback) => {
+      if (value==='') {
+        return callback(new Error('用户名不能为空'))
+      }else {
+        callback()
+      }
+    }
+    let validatePass = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请输入密码'))
+      } else {
+        callback()
+      }
+    }
+    let validateVerifyCode= (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请输入验证码'))
+      } else {
+        callback()
+      }
+    }
+    return {
+      loginForm: {
+        username: '',
+        password: '',
+        verifyCode: ''
+      },
+      loginRules: {
+        username: [
+          { validator: validateName, trigger: 'blur' }
+        ],
+        password: [
+          { validator: validatePass, trigger: 'blur' }
+        ],
+        verifyCode: [
+          { validator: validateVerifyCode, trigger: 'blur' }
+        ]
+      }
+    };
+  },
+  methods: {
+    submitForm(Form) {
+      this.$refs[Form].validate((valid) => {
+        if (valid) {
+          this.$router.push('Main')
+        } else {
+          console.log('error submit!!');
+          return false;
+        }
+      });
+    },
+    resetForm(Form) {
+      this.$refs[Form].resetFields();
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.login_container {
+  height: 100%;
+  background-color: #42b983;
+}
+
+.login_box {
+  width: 450px;
+  height: 380px;
+  background-color: #FFFFFF;
+  border-radius: 3px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+
+  .avatar_box {
+    width: 130px;
+    height: 130px;
+    border-radius: 50%;
+    border: 1px solid #eeeeee;
+    padding: 10px;
+    box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+    background-color: #ffffff;
+    margin: -65px auto;
+
+    img {
+      width: 100%;
+      height: 100%;
+      background-color: #eeeeee;
+      border-radius: 50%;
+    }
+  }
+
+  .login_form{
+    position: absolute;
+    bottom: 0px;
+    width: 100%;
+    padding: 0 20px;
+    box-sizing: border-box;
+
+    .login_btn{
+      display: flex;
+      justify-content: flex-end;
+    }
+
+    .verify_box{
+      display: flex;
+      .verify_code{
+        width: 60%;
+        justify-content: left;
+      }
+      .verify_img{
+        height: 45px;
+        width: 40%;
+        justify-content: flex-end;
+      }
+    }
+
+  }
+
+}
+</style>
---
+++ src/views/Main.vue
@@ -0,0 +1,157 @@
+<template>
+  <el-container class="main_container">
+    <el-header>
+      <!--logo和侧边栏控制-->
+      <div class="left_box">
+        <img src="../assets/img/索隆.gif" alt="头像">
+        <span>xxx系统</span>
+        <hamburger id="hamburger-container"  :is-active="opened" class="hamburger-container" @toggleClick="collapseStatus()" />
+      </div>
+      <!--头像-->
+      <div class="right_box">
+        <!--下拉菜单-->
+        <el-dropdown>
+          <img src="../assets/img/索隆.gif" alt="头像">
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item icon="el-icon-house">系统首页</el-dropdown-item>
+            <el-dropdown-item icon="el-icon-user">个人资料</el-dropdown-item>
+            <el-dropdown-item icon="el-icon-switch-button">退出登录</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+      </div>
+    </el-header>
+    <el-container>
+      <el-aside width="auto">
+        <el-menu default-active="1-4-1" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose"
+                 :collapse="isCollapse">
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span slot="title">导航一</span>
+            </template>
+            <el-menu-item-group>
+              <span slot="title">分组一</span>
+              <el-menu-item index="1-1">选项1</el-menu-item>
+              <el-menu-item index="1-2">选项2</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group title="分组2">
+              <el-menu-item index="1-3">选项3</el-menu-item>
+            </el-menu-item-group>
+            <el-submenu index="1-4">
+              <span slot="title">选项4</span>
+              <el-menu-item index="1-4-1">选项1</el-menu-item>
+            </el-submenu>
+          </el-submenu>
+          <el-menu-item index="2">
+            <i class="el-icon-menu"></i>
+            <span slot="title">导航二</span>
+          </el-menu-item>
+          <el-menu-item index="3" disabled>
+            <i class="el-icon-document"></i>
+            <span slot="title">导航三</span>
+          </el-menu-item>
+          <el-menu-item index="4">
+            <i class="el-icon-setting"></i>
+            <span slot="title">导航四</span>
+          </el-menu-item>
+        </el-menu>
+      </el-aside>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+import Hamburger from '../components/Hamburger/index.vue'
+export default {
+  name: 'Main',
+  components:{
+    Hamburger
+  },
+  data () {
+    return {
+      isCollapse: true,
+      opened: false
+    }
+  },
+  methods: {
+    handleOpen (key, keyPath) {
+      console.log(key, keyPath)
+    },
+    handleClose (key, keyPath) {
+      console.log(key, keyPath)
+    },
+    collapseStatus () {
+      this.isCollapse = !this.isCollapse
+      this.opened = !this.opened
+    },
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.main_container {
+  height: 100%;
+}
+
+.el-header {
+  background-color: #B3C0D1;
+  color: #333;
+  display: flex;
+  justify-content: space-between;
+  .left_box {
+    display: flex;
+    align-items: center;
+    font-size: 20px;
+    button{
+      size: 20px;
+      background-color: #B3C0D1;
+    }
+    img{
+      width: 60px;
+      height: 60px;
+    }
+  }
+
+  .right_box {
+    margin-right: 30px;
+    img{
+      width: 60px;
+      height: 60px;
+      border-radius: 50%;
+    }
+  }
+}
+
+.el-aside {
+  background-color: #D3DCE6;
+  color: #333;
+  text-align: center;
+  line-height: 200px;
+}
+
+.el-main {
+  background-color: #E9EEF3;
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+}
+
+.el-menu-vertical-demo:not(.el-menu--collapse) {
+  width: 200px;
+  min-height: 400px;
+}
+
+.hamburger-container {
+  line-height: 46px;
+  height: 100%;
+  float: left;
+  cursor: pointer;
+  transition: background .3s;
+  -webkit-tap-highlight-color:transparent;
+
+  &:hover {
+    background: rgba(0, 0, 0, .025)
+  }
+}
+</style>
