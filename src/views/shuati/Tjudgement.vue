55b98bc38fbde1b18eb292cb7d8e124c731b4b84
gloxxxxi <1169550631@qq.com>
2021-01-28 16:30:42 +0800

    vue 测试分支

---
+++ src/views/shuati/Tjudgement.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+  <el-button>ceshi</el-button>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "Tjudgement",
+
+    }
+</script>
+
+<style scoped>
+
+</style>
